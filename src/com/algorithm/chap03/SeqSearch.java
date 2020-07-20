4a3ae22e424b9c3072244dcd5029dffe65072995
yang <y2gcoder@gmail.com>
2020-07-20 23:57:46 +0900

    ~ p.114p

--- src/com/algorithm/chap03/SeqSearch.java
+++ src/com/algorithm/chap03/SeqSearch.java
@@ -6,13 +6,19 @@ class SeqSearch {
     static int seqSearch(int[] a, int n, int key) {
         int i = 0;
 
-        while (true) {
-            if (i == n)
-                return -1;
-            if (a[i] == key)
+//        while (true) {
+//            if (i == n)
+//                return -1;
+//            if (a[i] == key)
+//                return i;
+//            i++;
+//        }
+        while (i < n) {
+            if(a[i] == key)
                 return i;
             i++;
         }
+        return -1;
     }
 
     public static void main(String[] args) {
